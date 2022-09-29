f2eb29378bc09ac40fe6ed82bf9c245eb31a096d
DanielleMassey <danimassey1998@gmail.com>
2022-09-29 16:01:21 +0100

    Added array activity

--- src/com/qa/oop/practise/Main.java
+++ src/com/qa/oop/practise/Main.java
@@ -1,5 +1,7 @@
 package com.qa.oop.practise;
 
+import java.util.Arrays;
+
 public class Main {
 
 	public static void main(String[] args) {
@@ -20,11 +22,48 @@ public class Main {
 		
 		System.out.println(Product.sellerContactDetails(Product.SELLER_NAME, Product.CONTACT_NO, Product.EMAIL_ADDRESS));
 		
+		int[] numbers = {1,6,2,8,3,9,15,0};
+		System.out.println(sumArray(numbers));
+		System.out.println(smallestOfArray(numbers));
+		System.out.println(largestOfArray(numbers));
 		
 		
 			
 		}
-
+	
+	// 5.a. Method to return the sum of all array elements
+	static int sumArray(int[] numbers){
+		
+		
+		int total;
+		total = 0;
+		
+		for(int number : numbers)
+		{
+			total += number;
+			
+		}
+		return total;
+		
+	
 	}
+	
+	// 5.b.  Method to return the smallest number of the array elements
+	static int smallestOfArray(int[] numbers) {
+		Arrays.sort(numbers);
+		return numbers[0];
+	}
+	
+	// 5.c. Method to return the highest number of the array elements
+	static int largestOfArray(int[] numbers) {
+		Arrays.sort(numbers);
+		int n = numbers.length;
+		return numbers[n - 1];
+	}
+	
+		
+	}
+
+	
 
 
